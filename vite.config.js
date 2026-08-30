ce22d1f0be656b08db7c22f980ec7547ec6416e4
rzaragozasolis <ricardo.zaragoza@riac.dev>
2026-08-30 14:36:48 -0500

    feat: update marketing site

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ const base = process.env.VITE_BASE_PATH || '/';
 export default defineConfig({
   base,
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  },
 });
